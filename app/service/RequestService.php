b9d2f6e556bff5cfe633659c844b870238302c4d
driven-by-desire <123775319+driven-by-desire@users.noreply.github.com>
2023-06-12 00:42:49 +0530

    Update request

--- app/service/RequestService.php
+++ app/service/RequestService.php
@@ -4,34 +4,43 @@ namespace WebApp\Service;
 
 class RequestService
 {
-    private $request;
-    private $args = array();
+    private $request;   // array containing 1. header info 2. form-data info
+    private $args = array();     # future : check for non rest parameters in URI : if so give error
 
     public function  __construct(){
         //print_r($_SERVER); die();
-        //$this->request['header']            = $_SERVER            ;
-        $this->request['SERVER_PROTOCOL']   = $_SERVER['SERVER_PROTOCOL']   ;
-        $this->request['REQUEST_METHOD']    = $_SERVER['REQUEST_METHOD']    ;
-        $this->request['QUERY_STRING']      = $_SERVER['QUERY_STRING']      ;
-        $this->request['REQUEST_URI']       = $_SERVER['REQUEST_URI']       ;
-        $this->request['REQUEST_TIME_FLOAT'] = $_SERVER['REQUEST_TIME_FLOAT'] ;
-        $this->request['REQUEST_TIME']      = $_SERVER['REQUEST_TIME']      ;
-        //$this->request['HTTP_SEC_CH_UA']    = $_SERVER['HTTP_SEC_CH_UA']    ;
-        $this->request['HTTP_USER_AGENT']   = $_SERVER['HTTP_USER_AGENT']   ;
-        //$this->request['HTTP_COOKIE']       = $_SERVER['HTTP_COOKIE']       ;
-        $this->request['REMOTE_ADDR']       = $_SERVER['REMOTE_ADDR']       ;
-        $this->request['REQUEST_SCHEME']    = $_SERVER['REQUEST_SCHEME']    ;
+        $this->request['header'] = $this->getServerHeader();     // set header     
+        $this->request['_data'] = $this->getPostData();     // set header     
+
         $this->getRequest();
     }
 
     public function getRequest(){
-        if($this->request['REQUEST_METHOD'] == 'POST'){
-            $this->request['_data'] = $_POST;
-        }
         return $this->request;
     }
 
     public function getRequestMethod(){
-        return $this->request['REQUEST_METHOD'];
+        return $this->request['header']['REQUEST_METHOD'];
+    }
+
+    public function getPostData(){
+        $_postdata = $_POST;    # future : sanitize $_POST data as per int, str
+        $_data = ($this->getRequestMethod() == 'POST')? $_postdata : FALSE;
+        return $_data;
+    }
+
+    public function getServerHeader(){
+
+        $header['SERVER_PROTOCOL']   = $_SERVER['SERVER_PROTOCOL']   ;
+        $header['REQUEST_METHOD']    = $_SERVER['REQUEST_METHOD']    ;
+        $header['QUERY_STRING']      = $_SERVER['QUERY_STRING']      ;
+        $header['REQUEST_URI']       = $_SERVER['REQUEST_URI']       ;
+        $header['REQUEST_TIME_FLOAT'] = $_SERVER['REQUEST_TIME_FLOAT'] ;
+        $header['REQUEST_TIME']      = $_SERVER['REQUEST_TIME']      ;
+        $header['HTTP_USER_AGENT']   = $_SERVER['HTTP_USER_AGENT']   ;
+        $header['REMOTE_ADDR']       = $_SERVER['REMOTE_ADDR']       ;
+        $header['REQUEST_SCHEME']    = $_SERVER['REQUEST_SCHEME']    ;
+
+        return $header;
     }
 }
